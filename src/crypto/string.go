21e5394873fb9795ce2b5c2a3afab16fd2692038
Starz0r <starz0r@starz0r.com>
2021-07-26 23:52:07 -0500

    crypto: string function that only includes letters and numbers

--- src/crypto/string.go
+++ src/crypto/string.go
@@ -1,7 +1,8 @@
 package crypto
 
 const (
-	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._-"
+	letterBytes  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._-"
+	letterBytes2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 	letterIdxBits = 7 // 7 bits to represent a Letter index
 	letterIdxMask = 1<<letterIdxBits - 1
@@ -18,6 +19,24 @@ func String(n int) string {
 		}
 		if idx := int(cache & letterIdxMask); idx < l {
 			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func StringReadable(n int) string {
+	b := make([]byte, n)
+	l := len(letterBytes2)
+	for i, cache, remain := n-1, randpool.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = randpool.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < l {
+			b[i] = letterBytes2[idx]
 			i--
 		}
 		cache >>= letterIdxBits
